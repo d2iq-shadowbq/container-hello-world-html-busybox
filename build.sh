81ba5fbde66b6ea97c9ce0463a8a5ccb75d07ff1
shadowbq <shadowbq@gmail.com>
2020-03-09 15:34:32 -0400

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-docker build -t hello-world .
+#!/usr/bin/env bash
+
+docker build -t "hello-world:$(cat ./VERSION)" .
 docker run -p 80:8005 hello-world
 ## open your browser and check http://localhost/
 docker login
-docker tag hello-world {your_dockerhub_user}/hello-world
-docker push {your_dockerhub_user}/hello-world:latest
+docker tag hello-world d2iqshadowbq/hello-world
+docker push d2iqshadowbq/hello-world:latest
