d7faf525cdfdde7edfd44b3e2cf3d1c7ddd466d2
d2iq-shadowbq <smacgregor@d2iq.com>
2020-03-09 16:20:23 -0400

    fix build

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ _APP_VERSION=$(cat ./VERSION)
 docker login
 
 docker build -t "hello-world:$_APP_VERSION" .
-docker run -p 80:8005 hello-world
+docker rm hello_world_test > /dev/null 2>&1
+docker run -p 80:8005 --name hello_world_test hello-world > /dev/null 2>&1 &
+sleep 5
 curl -s http:///localhost |grep 'Hello World'
+docker kill hello_world_test
+docker rm hello_world_test > /dev/null 2>&1
